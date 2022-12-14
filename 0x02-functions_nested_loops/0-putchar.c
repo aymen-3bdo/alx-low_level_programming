a791b3b773ab21e86a338b1a865df67cb286c50b
aymen-3bdo <aymenderderi@gmail.comom>
2022-12-14 13:01:23 -0800

    c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+ * main - Entry point
+ *
+ * Return: return 0
+ *
+ */
+int main(void)
+{
+  char name[] = "_putchar";
+  int i;
+
+  for (i = 0; i < 8; i++)
+    {
+      _putchar(name[i]);
+    }
+  _putchar('\n');
+  return (0);
+}
